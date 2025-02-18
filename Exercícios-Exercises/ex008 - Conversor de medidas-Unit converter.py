339951f42238085a81386c1c0fbb9d0e9f3164c5
Poliana Brito <polianabritofidelis@gmail.com>
2025-02-18 13:59:48 -0300

    correções e complementos 180225 1359

--- Exercícios-Exercises/ex008 - Conversor de medidas-Unit converter.py
+++ Exercícios-Exercises/ex008 - Conversor de medidas-Unit converter.py
@@ -1,6 +1,18 @@
-#Escreva um programa que leia um valor em metros, e exiba-o convertido em centímetros e milímetros
+#Escreva um programa que leia um valor em metros, e exiba-o convertido em quilômetros, hectômetros, decâmetros, decímetros, centímetros e milímetros
+
+#1m=10dc
 #1m=100cm
 #1m=1000mm
 
-metros=float(input('Digite a metragem: '))
-print('{} em centímetros: {:.0f} \n{} em milímetros: {:.0f}'.format(metros, metros*100, metros, metros*1000))
+m = float(input('Digite a metragem: '))
+
+km= m/1000
+hm= m/100
+dam= m/10
+dm= m*10
+cm = m*100
+mm= m*1000
+
+print('-'*20)
+print('Tabela de medidas baseada em {}m:'.format(m))
+print('{}m = {}km \n{}m = {}hm \n{}m = {}dam \n{}m = {}dm \n{}m = {}cm \n{}m = {}mm'.format(m,km, m, hm, m, dam, m, dm, m, cm, m, mm))
