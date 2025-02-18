eb64f5097206a1d6e32f42b188a009b24a4c2f68
Poliana Brito <polianabritofidelis@gmail.com>
2025-02-18 14:04:38 -0300

    att

--- Exercícios-Exercises/ex006 - Dobro triplo raiz quadrada-Double triple square root.py
+++ Exercícios-Exercises/ex006 - Dobro triplo raiz quadrada-Double triple square root.py
@@ -2,3 +2,5 @@
 
 num1=int(input('Digite um número para descobrir seu dobro, triplo e raiz quadrada: '))
 print('O dobro de {} é {}, o triplo é {}, e a raiz quadrada é {:.1f}'.format(num1, num1*2, num1*3, num1**(1/2)))
+
+#num1**(1/2) também pode ser feito com pow(num1, (1/2)
--- Exercícios-Exercises/ex008 - Conversor de medidas-Unit converter.py
+++ Exercícios-Exercises/ex008 - Conversor de medidas-Unit converter.py
@@ -4,6 +4,7 @@
 #1m=100cm
 #1m=1000mm
 
+<<<<<<< Updated upstream
 m = float(input('Digite a metragem: '))
 
 km= m/1000
@@ -16,3 +17,5 @@ mm= m*1000
 print('-'*20)
 print('Tabela de medidas baseada em {}m:'.format(m))
 print('{}m = {}km \n{}m = {}hm \n{}m = {}dam \n{}m = {}dm \n{}m = {}cm \n{}m = {}mm'.format(m,km, m, hm, m, dam, m, dm, m, cm, m, mm))
+=======
+>>>>>>> Stashed changes
