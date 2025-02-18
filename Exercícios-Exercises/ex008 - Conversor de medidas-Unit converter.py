f63390a092baf671f7018a03c7047a48cda3baf5
Poliana Brito <polianabritofidelis@gmail.com>
2025-02-18 14:05:48 -0300

    Update ex008 - Conversor de medidas-Unit converter.py

--- Exercícios-Exercises/ex008 - Conversor de medidas-Unit converter.py
+++ Exercícios-Exercises/ex008 - Conversor de medidas-Unit converter.py
@@ -4,7 +4,6 @@
 #1m=100cm
 #1m=1000mm
 
-<<<<<<< Updated upstream
 m = float(input('Digite a metragem: '))
 
 km= m/1000
@@ -17,5 +16,3 @@ mm= m*1000
 print('-'*20)
 print('Tabela de medidas baseada em {}m:'.format(m))
 print('{}m = {}km \n{}m = {}hm \n{}m = {}dam \n{}m = {}dm \n{}m = {}cm \n{}m = {}mm'.format(m,km, m, hm, m, dam, m, dm, m, cm, m, mm))
-=======
->>>>>>> Stashed changes
